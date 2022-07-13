ebcaa481d8674ae6b7decad4767a577f0afea76a
nyirurugo omar <nyirurugoomar@gmail.com>
2022-07-13 18:48:42 +0200

    pricing and footer sections

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Support from "./components/Support";
 import AllInOne from "./components/AllInOne";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <About />
       <Support />
       <AllInOne />
+      <Pricing/>
+      <Footer/>
       
     </>
   );
