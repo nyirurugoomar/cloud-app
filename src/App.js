9419c86ead3f1d6121bb76359d73712f218fd814
nyirurugo omar <nyirurugoomar@gmail.com>
2022-07-13 18:33:17 +0200

    allin one section

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Support from "./components/Support";
+import AllInOne from "./components/AllInOne";
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Support/>
-
-
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+      <AllInOne />
+      
     </>
   );
 }
